8ed9bb43ca738f80989e44e4e578f31fd7b321ea
locoste <louis.coste2@live.fr>
2020-12-07 18:40:50 +0100

    .

--- Groupe analyse/Prediction/FBProphet.py
+++ Groupe analyse/Prediction/FBProphet.py
@@ -5,6 +5,7 @@ import mpld3
 import pickle
 import pandas as pd
 import FBMethods as fm
+import matplotlib.pyplot as plt
 from fbprophet import Prophet
 from pandas import to_datetime
 
@@ -29,20 +30,19 @@ def get_topics(filename):
         _topics[row['topic']] = row['tokens']
     return _topics
 
-def generate_graphs(topic_number, topics_filename, publication_filename):
-    pd.set_option('mode.chained_assignment', None)
-    pd.set_option('display.max_colwidth', None)
-    df = pd.read_csv(publication_filename, sep = ',', encoding = 'cp1252')
+def generate_graphs(topic_number, topics_filename, id2date_filename):
+    global id2date
+    id2date = load(id2date_filename)
     data = load(topics_filename)
     cleaned_data = data.reset_index()
     cleaned_data = cleaned_data.rename(columns={'Titles_ID':'id_publication'})
-    final_data = pd.merge(df, cleaned_data, on='id_publication')
+    final_data = pd.merge(id2date, cleaned_data, on='id_publication')
     final_data = final_data[['date_pub', 'Dominant_Topic']]
     final_data.columns = ['date', 'topic']
     final_data['topic'] = final_data['topic'].astype(int)
 
     future = list()
-    for i in range(19,21):
+    for i in range(19,22):
         for j in range(1,13):
             date = '20%s-%s' % (str(i), str(j))
             future.append([date])
@@ -56,7 +56,7 @@ def generate_graphs(topic_number, topics_filename, publication_filename):
     topic_data = topic_data.groupby('date').count()
     topic_data = topic_data.reset_index()
     topic_data.columns = ['ds', 'y']
-    #print(topic_data.head())
+    topic_data = topic_data[~(topic_data['ds'] < '2017-01-01')]
     model = Prophet(weekly_seasonality=False, daily_seasonality=False)
     model.fit(topic_data)
     
@@ -70,15 +70,19 @@ def generate_graphs(topic_number, topics_filename, publication_filename):
     plot_data['forecast'] = forecast
     plot_data['history'] = model.history
     fig = fm.plot_components(plot_data)
+    #fig = model.plot(forecast)
+    #model.plot_components(forecast)
+    plt.savefig('prophet')
     mpld3.save_html(fig, "figures/figure_%s.html" % topic_number)
     
 
 ## Utilisation :
 # Récupération des topics =>
-topics = get_topics('pickle/topic_davy')
+topics = get_topics('pickle/df_topic')
 # Génération du html =>
-generate_graphs(
-    topic_number = 0,
-    topics_filename = 'pickle/topic_davy',
-    publication_filename = 'data/publication.csv'
-)
+# for i in range(len(topics)):
+#     generate_graphs(
+#         topic_number = i,
+#         topics_filename = 'pickle/df_topic',
+#         id2date_filename = 'pickle/df_new_data'
+#     )
